269bc5ad7a64f334ff7dd6a1f05b6131111d844e
spectreseven1138 <44363049+spectreseven1138@users.noreply.github.com>
2022-09-14 17:05:32 +0100

    Remove igo

--- app/src/main/java/com/spectre7/spmp/ui/components/LyricsDisplay.kt
+++ app/src/main/java/com/spectre7/spmp/ui/components/LyricsDisplay.kt
@@ -25,8 +25,8 @@ import net.zerotask.libraries.android.compose.furigana.TextData
 import net.zerotask.libraries.android.compose.furigana.TextWithReading
 import java.io.BufferedReader
 import java.io.InputStreamReader
-import net.reduls.igo.Morpheme
-import net.reduls.igo.Tagger
+// import net.reduls.igo.Morpheme
+// import net.reduls.igo.Tagger
 import java.io.*;
 import java.net.*;
 import java.nio.file.*;
@@ -101,51 +101,29 @@ fun prepareIgoDict(): String {
     return dict.absolutePath
 }
 
-// val kakasi = Python.getInstance().getModule("pykakasi").callAttr("Kakasi")
+val kakasi = Python.getInstance().getModule("pykakasi").callAttr("Kakasi")
 
 @Composable
 fun FuriganaText(text: String, show_furigana: Boolean, trim_okurigana: Boolean = true) {
 
-    // TODO | Consider replacing Kakasi with Igo
     fun generateContent(text: String): MutableList<TextData> {
 
         val content: MutableList<TextData> = mutableStateListOf<TextData>()
 
-        data class Term(val orig: String, val furi: String, kata: String) {
-            val changed: Boolean = orig != furi && orig != kata
-        }
+        for (term in kakasi.callAttr("convert", text.replace("\n", "\\n").replace("\r", "\\r")).asList()) {
 
-        fun getTerms(text: String): List<Term> {
-
-            // Igo
-            val terms = Tagger(prepareIgoDict()).parse(text)
-            
-            // Kakasi
-            // val terms = kakasi.callAttr("convert", text.replace("\n", "\\n").replace("\r", "\\r")).asList()
-
-            val ret = List(terms.size) {
-                val term = terms[i]
-
-                // Igo
-                Term(term.surface, kata.toHiragana(), term.feature.split(",", 8)[7])
-
-                // Kakasi
-                // fun getKey(key: String): String {
-                //     return term.callAttr("get", key).toString().replace("\\n", "\n").replace("\\r", "\r")
-                // }
-                // Term(getKey("orig"), getKey("hira"), getKey("kana"))
+            fun getKey(key: String): String {
+                return term.callAttr("get", key).toString().replace("\\n", "\n").replace("\\r", "\r")
             }
-            
-            return ret
-        }
 
-        for (term in getTerms(text)) {
+            val orig = getKey("orig")
+            val hira = getKey("hira")
 
-            if (term.changed) {
-                if (trim_okurigana && term.orig.hasKanjiAndHiragana()) {
+            if (orig != hira && orig != getKey("kana")) {
+                if (trim_okurigana && orig.hasKanjiAndHiragana()) {
                     var trim_amount: Int = 0
-                    for (i in 1 until term.furi.length + 1) {
-                        if (term.orig[term.orig.length - i].isKanji() || term.orig[term.orig.length - i] != term.furi[term.furi.length - i]) {
+                    for (i in 1 until hira.length + 1) {
+                        if (orig[orig.length - i].isKanji() || orig[orig.length - i] != hira[hira.length - i]) {
                             trim_amount = i - 1
                             break
                         }
@@ -153,26 +131,26 @@ fun FuriganaText(text: String, show_furigana: Boolean, trim_okurigana: Boolean =
 
                     if (trim_amount != 0) {
                         content.add(TextData(
-                            text = term.orig.slice(0 until term.orig.length - trim_amount),
-                            reading = term.furi.slice(0 until term.furi.length - trim_amount)
+                            text = orig.slice(0 until orig.length - trim_amount),
+                            reading = hira.slice(0 until hira.length - trim_amount)
                         ))
 
                         content.add(TextData(
-                            text = term.orig.slice(term.orig.length - trim_amount until term.orig.length),
+                            text = orig.slice(orig.length - trim_amount until orig.length),
                             reading = null
                         ))
                     }
                 }
                 else {
                     content.add(TextData(
-                        text = term.orig,
-                        reading = term.furi
+                        text = orig,
+                        reading = hira
                     ))
                 }
             }
             else {
                 content.add(TextData(
-                    text = term.orig,
+                    text = orig,
                     reading = null
                 ))
             }

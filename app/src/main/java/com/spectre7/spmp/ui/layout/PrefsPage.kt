7c2578a7f894784514f644e0cabe075e4819d0bf
spectreseven1138 <44363049+spectreseven1138@users.noreply.github.com>
2022-10-01 00:34:07 +0100

    Lyrics / settings progress

--- app/src/main/java/com/spectre7/spmp/ui/layout/PrefsPage.kt
+++ app/src/main/java/com/spectre7/spmp/ui/layout/PrefsPage.kt
@@ -16,11 +16,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import com.spectre7.composesettings.ui.*
 import com.spectre7.composesettings.model.*
 import androidx.activity.compose.BackHandler
+import androidx.compose.ui.unit.*
 import androidx.compose.ui.graphics.Color
 import com.spectre7.spmp.MainActivity
 import com.spectre7.utils.Theme
 import androidx.compose.ui.zIndex
 import androidx.compose.ui.input.pointer.pointerInput
+import com.spectre7.spmp.PlayerHost
 
 enum class Page { ROOT, OTHER }
 
@@ -28,31 +30,30 @@ enum class Page { ROOT, OTHER }
 fun PrefsPage(setOverlayPage: (page: OverlayPage) -> Unit) {
 
     val slider_state = remember { SettingsValueState(0.5f, "slider", MainActivity.prefs) }
+    val settings_interface: SettingsInterface = remember { SettingsInterface(MainActivity.getTheme(), Page.ROOT.ordinal, {
+        when (Page.values()[it]) {
+            Page.ROOT -> SettingsPage("Preferences", listOf(
+                SettingsGroup("Theming"),
+                SettingsItemToggle(SettingsValueState(false, "toggle", MainActivity.prefs), "Hello World", "Subtitle text text subtitle text"),
+                SettingsItemSubpage("Other page", "Subtitle!", Page.OTHER.ordinal),
+                SettingsItemSlider(slider_state, "Hello World - ${slider_state.value}", "Subtitle text text subtitle text"),
+            ), Modifier.fillMaxSize())
+            Page.OTHER -> SettingsPage("Other Preferences", listOf(
+                SettingsGroup("Theming"),
+                SettingsItemToggle(SettingsValueState(false, "toggle", MainActivity.prefs), "Hello World", "Subtitle text text subtitle text"),
+                SettingsItemSlider(slider_state, "Hello World - ${slider_state.value}", "Subtitle text text subtitle text"),
+                SettingsItemSubpage("Root page", "Other subtitle!", Page.ROOT.ordinal)
+            ), Modifier.fillMaxSize())
+        }
+    }, {
+        setOverlayPage(OverlayPage.NONE)
+    }) }
 
-    Box(Modifier.pointerInput(Unit) {}) {
-        SettingsInterface(MainActivity.getTheme(), Page.ROOT, {
-            when (it) {
-                Page.ROOT -> SettingsPage("Preferences", listOf(
-                    SettingsGroup("Theming"),
-                    SettingsItemToggle(SettingsValueState(false, "toggle", MainActivity.prefs), "Hello World", "Subtitle text text subtitle text"),
-                    SettingsItemSlider(slider_state, "Hello World - ${slider_state.value}", "Subtitle text text subtitle text"),
-                    SettingsItemSubpage("Other page", "Subtitle!", Page.OTHER)
-                ), Modifier.fillMaxSize().background(MainActivity.getTheme().getBackground(false)))
-                Page.OTHER -> SettingsPage("Other Preferences", listOf(
-                    SettingsGroup("Theming"),
-                    SettingsItemToggle(SettingsValueState(false, "toggle", MainActivity.prefs), "Hello World", "Subtitle text text subtitle text"),
-                    SettingsItemSlider(slider_state, "Hello World - ${slider_state.value}", "Subtitle text text subtitle text"),
-                    SettingsItemSubpage("Root page", "Other subtitle!", Page.ROOT)
-                ), Modifier.fillMaxSize().background(MainActivity.getTheme().getBackground(false)))
-            }
-        }).Interface()
+    Box(Modifier.background(MainActivity.getTheme().getBackground(false)).padding(20.dp).pointerInput(Unit) {}) {
+        settings_interface.Interface()
     }
 
-    BackHandler {
-        PlayerHost.interact {
-            // Volume adjustment test
-            it.setVolume(it.getVolume() + 0.1)
-        }
-        // setOverlayPage(OverlayPage.NONE)
+    PlayerHost.interact {
+        it.setVolume(it.getVolume() * 0.95f)
     }
 }

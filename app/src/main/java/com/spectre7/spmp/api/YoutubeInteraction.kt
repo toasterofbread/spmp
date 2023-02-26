14b5ead62f98f1f05d6d03675a9d620a3c18fc64
spectreseven1138 <spectreseven1138@gmail.com>
2023-02-26 23:00:00 +0000

    Add like/dislike button to NowPlaying

--- app/src/main/java/com/spectre7/spmp/api/YoutubeInteraction.kt
+++ app/src/main/java/com/spectre7/spmp/api/YoutubeInteraction.kt
@@ -12,11 +12,7 @@ fun isSubscribedToArtist(artist: Artist): Result<Boolean?> {
     val request: Request = Request.Builder()
         .url("https://music.youtube.com/youtubei/v1/browse")
         .headers(DataApi.getYTMHeaders())
-        .post(DataApi.getYoutubeiRequestBody("""
-                    {
-                        "browseId": "${artist.id}"
-                    }
-                """))
+        .post(DataApi.getYoutubeiRequestBody("""{ "browseId": "${artist.id}" }"""))
         .build()
 
     val result = DataApi.request(request)
@@ -68,6 +64,63 @@ fun subscribeOrUnsubscribeArtist(artist: Artist, subscribe: Boolean): Result<Any
     return DataApi.request(request)
 }
 
+private data class PlayerLikeResponse(
+    val playerOverlays: PlayerOverlays
+) {
+    val status: LikeButtonRenderer get() = playerOverlays.playerOverlayRenderer.actions.single().likeButtonRenderer
+
+    class PlayerOverlays(val playerOverlayRenderer: PlayerOverlayRenderer)
+    data class PlayerOverlayRenderer(val actions: List<Action>)
+    data class Action(val likeButtonRenderer: LikeButtonRenderer)
+    data class LikeButtonRenderer(val likeStatus: String, val likesAllowed: Boolean)
+}
+
+fun getSongLiked(id: String): Result<Boolean?> {
+    val request: Request = Request.Builder()
+        .url("https://music.youtube.com/youtubei/v1/next")
+        .headers(DataApi.getYTMHeaders())
+        .post(DataApi.getYoutubeiRequestBody("""
+            {
+                "videoId": "$id"
+            }
+        """))
+        .build()
+
+    val result = DataApi.request(request)
+    if (result.isFailure) {
+        return result.cast()
+    }
+
+    val stream = result.getOrThrow().body!!.charStream()
+    val parsed: PlayerLikeResponse = DataApi.klaxon.parse(stream)!!
+    stream.close()
+
+    return Result.success(when (parsed.status.likeStatus) {
+        "LIKE" -> true
+        "DISLIKE" -> false
+        "INDIFFERENT" -> null
+        else -> throw NotImplementedError(parsed.status.likeStatus)
+    })
+}
+
+fun setSongLiked(id: String, liked: Boolean?): Result<Any> {
+    val request: Request = Request.Builder()
+        .url("https://music.youtube.com/youtubei/v1/" + when (liked) {
+            true -> "like/like"
+            false -> "like/dislike"
+            null -> "like/removelike"
+        })
+        .headers(DataApi.getYTMHeaders())
+        .post(DataApi.getYoutubeiRequestBody("""
+            {
+                "target": { "videoId": "$id" }
+            }
+        """))
+        .build()
+
+    return DataApi.request(request)
+}
+
 private data class PlaybackTrackingRepsonse(
     val playbackTracking: PlaybackTracking
 ) {
@@ -79,14 +132,16 @@ private data class PlaybackTrackingRepsonse(
     data class TrackingUrl(val baseUrl: String)
 }
 
+private const val CPN_ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_"
+private fun generateCpn(): String {
+    return (0 until 16).map { CPN_ALPHABET[Random().nextInt(256) and 63] }.joinToString("")
+}
+
 fun markSongAsWatched(id: String): Result<Any> {
     fun buildRequest(alt: Boolean): Request {
         return Request.Builder()
             .url("https://music.youtube.com/youtubei/v1/player?key=${getString(R.string.yt_i_api_key)}")
-            .post(DataApi.getYoutubeiRequestBody(body=
-                """{
-                    "videoId": "$id"
-                }""", alt))
+            .post(DataApi.getYoutubeiRequestBody("""{ "videoId": "$id" }""", alt))
             .headers(DataApi.getYTMHeaders())
             .build()
     }
@@ -100,15 +155,10 @@ fun markSongAsWatched(id: String): Result<Any> {
     val data: PlaybackTrackingRepsonse = DataApi.klaxon.parse(stream)!!
     stream.close()
 
-    val playback_data = data.playback_url.toHttpUrl()
-
-    val cpn_alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_"
-    val cpn = (0 until 16).map { cpn_alphabet[Random().nextInt(256) and 63] }.joinToString("")
-
-    val playback_url = playback_data.newBuilder()
+    val playback_url = data.playback_url.toHttpUrl().newBuilder()
         .setQueryParameter("ver", "2")
         .setQueryParameter("c", "WEB_REMIX")
-        .setQueryParameter("cpn", cpn)
+        .setQueryParameter("cpn", generateCpn())
         .build()
 
     val request = Request.Builder()
---
+++ app/src/main/java/com/spectre7/spmp/ui/component/LikeDislikeButton.kt
@@ -0,0 +1,97 @@
+package com.spectre7.spmp.ui.component
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.core.Animatable
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LineAxis
+import androidx.compose.material.icons.filled.QuestionMark
+import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.material.icons.outlined.ThumbUp
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.spectre7.spmp.api.getOrThrowHere
+import com.spectre7.spmp.api.getSongLiked
+import com.spectre7.spmp.api.setSongLiked
+import com.spectre7.spmp.model.Song
+import com.spectre7.utils.OnChangedEffect
+import com.spectre7.utils.vibrateShort
+import kotlin.concurrent.thread
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun LikeDislikeButton(
+    song: Song,
+    colour: Color
+) {
+    var loaded: Boolean by remember { mutableStateOf(false) }
+    var liked: Boolean? by remember { mutableStateOf(null) }
+    LaunchedEffect(song) {
+        loaded = false
+        thread {
+            liked = getSongLiked(song.id).getOrThrowHere()
+            loaded = true
+        }
+    }
+
+    val rotation = remember { Animatable(0f) }
+    OnChangedEffect(liked) {
+        rotation.animateTo(if (liked == false) 180f else 0f)
+    }
+
+    fun setLiked(value: Boolean?) {
+        if (!loaded) {
+            return
+        }
+
+        thread {
+            if (setSongLiked(song.id, value).isSuccess) {
+                liked = value
+            }
+            else {
+                liked = getSongLiked(song.id).getOrThrowHere()
+            }
+        }
+    }
+
+    Crossfade(liked != null) { active ->
+        Box(
+            Modifier.width(40.dp).fillMaxHeight(),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                if (active) Icons.Filled.ThumbUp else Icons.Outlined.ThumbUp,
+                null,
+                Modifier
+                    .rotate(rotation.value)
+                    .combinedClickable(
+                        onClick = { when (liked) {
+                            true -> setLiked(null)
+                            false -> setLiked(null)
+                            null -> setLiked(true)
+                        }},
+                        onLongClick = {
+                            when (liked) {
+                                true -> setLiked(false)
+                                false -> setLiked(true)
+                                null -> setLiked(false)
+                            }
+                            vibrateShort()
+                        }
+                    ),
+                tint = colour
+            )
+        }
+    }
+}

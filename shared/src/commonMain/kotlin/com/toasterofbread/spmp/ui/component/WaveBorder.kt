fca5f41877fce17ebe97c2009bb0a42a04d76ec0
Talo Halton <talohalton@gmail.com>
2023-11-06 00:33:21 +0000

    Use Shape instead of Canvas in WaveBorder

--- shared/src/commonMain/kotlin/com/toasterofbread/spmp/ui/component/WaveBorder.kt
+++ shared/src/commonMain/kotlin/com/toasterofbread/spmp/ui/component/WaveBorder.kt
@@ -1,28 +1,47 @@
 package com.toasterofbread.spmp.ui.component
 
 import LocalPlayerState
-import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.material.LocalContentColor
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.ClipOp
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.drawscope.clipPath
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import com.toasterofbread.spmp.ui.layout.apppage.mainpage.PlayerState
 import com.toasterofbread.toastercomposetools.settings.ui.Theme
-import getPlatform
 import kotlin.math.ceil
 import kotlin.math.roundToInt
 
 const val WAVE_BORDER_HEIGHT_DP: Float = 20f
+
+class WaveShape(val waves: Int, val offset: Float): Shape {
+    override fun createOutline(size: Size, layoutDirection: LayoutDirection, density: Density): Outline {
+        val path: Path = Path()
+
+        path.addRect(Rect(0f, 0f, size.width, size.height / 2))
+
+        wavePath(path, size, 1, waves) { offset }
+        wavePath(path, size, -1, waves) { offset }
+
+        return Outline.Generic(path)
+    }
+}
 
 @Composable
 fun WaveBorder(
@@ -31,94 +50,70 @@ fun WaveBorder(
     height: Dp = WAVE_BORDER_HEIGHT_DP.dp,
     getOffset: ((height: Int) -> Int)? = null,
     waves: Int = 3,
-    getWaveOffset: (DrawScope.() -> Float)? = null,
+    getWaveOffset: (Density.() -> Float)? = null,
     border_thickness: Dp = 0.dp,
-    border_colour: Color = LocalContentColor.current,
+    border_colour: Color = LocalContentColor.current
 ) {
-    val player = LocalPlayerState.current
+    val player: PlayerState = LocalPlayerState.current
+    val density: Density = LocalDensity.current
 
-    Canvas(
-        modifier
-            .fillMaxWidth()
-            .offset {
-                IntOffset(
-                    0,
-                    getOffset?.invoke(height.toPx().roundToInt()) ?: 0
+    val colour: Color = getColour(player.theme)
+    val offset: Float? = getWaveOffset?.invoke(density)
+    val shape: WaveShape = remember(waves, offset) {
+        WaveShape(waves, offset ?: 0f)
+    }
+
+    Box(modifier.fillMaxWidth().requiredHeight(0.dp)) {
+        Box(
+            Modifier
+                .fillMaxWidth()
+                .requiredHeight(height)
+                .offset(
+                    0.dp,
+                    with(density) {
+                        (getOffset?.invoke(height.toPx().roundToInt())?.toDp() ?: 0.dp) + (height / 2) - 1.toDp() + border_thickness
+                    }
                 )
-            }
-    ) {
-        // TODO
-        if (getPlatform() == Platform.DESKTOP) {
-            return@Canvas
-        }
-
-        val path = Path()
-        val colour = getColour(player.theme)
-
-        // Above equilibrium (cut out from rect)
-        wavePath(path, -1, height.toPx(), waves, getWaveOffset)
-        clipPath(
-            path,
-            ClipOp.Difference
-        ) {
-            drawRect(
-                colour,
-                topLeft = Offset(0f, height.toPx() / 2)
-            )
-        }
-
-        val border_stroke = if (border_thickness > 0.dp) Stroke(border_thickness.toPx()) else null
-
-        // Upper border
-        if (border_stroke != null) {
-            drawPath(path, border_colour, style = border_stroke)
-        }
-
-        // Below equilibrium
-        wavePath(path, 1, height.toPx(), waves, getWaveOffset)
-        drawPath(path, colour)
-
-        // Lower border
-        if (border_stroke != null) {
-            drawPath(path, border_colour, style = border_stroke)
-        }
+                .background(border_colour, shape)
+                .offset(0.dp, -border_thickness)
+                .background(colour, shape)
+        )
     }
 }
 
-fun DrawScope.drawWave(
+inline fun DrawScope.drawWave(
     waves: Int,
-    height: Float = size.height,
+    wave_size: Size = size,
     stroke_width: Float = 2f,
-    getWaveOffset: (DrawScope.() -> Float)? = null,
-    getColour: DrawScope.() -> Color,
+    getWaveOffset: () -> Float,
+    getColour: () -> Color,
 ) {
     val path = Path()
     val colour = getColour()
     val stroke = Stroke(stroke_width)
 
     // Above equilibrium
-    wavePath(path, -1, height, waves, getWaveOffset)
+    wavePath(path, wave_size, -1, waves, getWaveOffset)
     drawPath(path, colour, style = stroke)
+    path.reset()
 
     // Below equilibrium
-    wavePath(path, 1, height, waves, getWaveOffset)
+    wavePath(path, wave_size, 1, waves, getWaveOffset)
     drawPath(path, colour, style = stroke)
 }
 
-private fun DrawScope.wavePath(
+inline fun wavePath(
     path: Path,
+    size: Size,
     direction: Int,
-    height: Float,
     waves: Int,
-    getOffset: (DrawScope.() -> Float)? = null
+    getOffset: () -> Float
 ): Path {
-    path.reset()
-
-    val y_offset = height / 2
-    val half_period = (size.width / (waves - 1)) / 2
-    val offset_px = getOffset?.invoke(this)?.let { offset ->
+    val y_offset: Float = size.height / 2
+    val half_period: Float = (size.width / (waves - 1)) / 2
+    val offset_px: Float = getOffset().let { offset ->
         offset % size.width - (if (offset > 0f) size.width else 0f)
-    } ?: 0f
+    }
 
     path.moveTo(x = -half_period / 2 + offset_px, y = y_offset)
 
@@ -130,7 +125,7 @@ private fun DrawScope.wavePath(
 
         path.relativeQuadraticBezierTo(
             dx1 = half_period / 2,
-            dy1 = height / 2 * direction,
+            dy1 = size.height / 2 * direction,
             dx2 = half_period,
             dy2 = 0f,
         )

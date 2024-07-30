5491d4a50e68b3ba4296062eeef863f0edac042e
toasterofbread <talohalton@gmail.com>
2024-07-30 18:55:07 +0100

    Use ytm-kt v0.3.0

--- buildSrc/src/main/kotlin/plugins/spmp/Dependencies.kt
+++ buildSrc/src/main/kotlin/plugins/spmp/Dependencies.kt
@@ -44,7 +44,7 @@ class SpMpDeps(extra: Map<String, Any>) {
                 license_url = "https://github.com/toasterofbread/ComposeKit/blob/136f216e65395660255d3270af9b79c90ae2254c/LICENSE"
             ),
             "dev.toastbits.ytmkt" to DependencyInfo(
-                version = "0.2.6-SNAPSHOT",
+                version = "0.3.0",
                 name = "ytm-kt",
                 author = "toasterofbread",
                 url = "https://github.com/toasterofbread/ytm-kt",

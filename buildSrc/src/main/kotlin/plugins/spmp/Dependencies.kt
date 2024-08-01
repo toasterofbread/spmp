919bef6ccf9222a4e2da1976fd2cf7de6a6c2b44
toasterofbread <talohalton@gmail.com>
2024-08-01 19:52:55 +0100

    Update list of illegal filename chars on Android (closes #244)

--- buildSrc/src/main/kotlin/plugins/spmp/Dependencies.kt
+++ buildSrc/src/main/kotlin/plugins/spmp/Dependencies.kt
@@ -36,7 +36,7 @@ class SpMpDeps(extra: Map<String, Any>) {
                 license_url = "https://github.com/toasterofbread/spmp-server/blob/6dde651ffc102d604ac7ecd5ac7471b1572fd2e6/LICENSE"
             ),
             "dev.toastbits.composekit" to DependencyInfo(
-                version = "dff1048c84",
+                version = "418c176707",
                 name = "ComposeKit",
                 author = "toasterofbread",
                 url = "https://github.com/toasterofbread/composekit",

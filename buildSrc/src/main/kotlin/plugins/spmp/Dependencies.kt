01ee4789823416ac3db47e7bb977e04ad32ab3fd
toasterofbread <talohalton@gmail.com>
2024-09-11 20:35:49 +0100

    Bump SpMs version to 0.4.1

--- buildSrc/src/main/kotlin/plugins/spmp/Dependencies.kt
+++ buildSrc/src/main/kotlin/plugins/spmp/Dependencies.kt
@@ -28,7 +28,7 @@ class SpMpDeps(extra: Map<String, Any>) {
     val dependencies: Map<String, DependencyInfo> =
         mapOf(
             "dev.toastbits:spms" to DependencyInfo(
-                version = "0.4.0-beta2",
+                version = "0.4.1",
                 name = "spmp-server",
                 author = "toasterofbread",
                 url = "https://github.com/toasterofbread/spmp-server",

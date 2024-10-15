0275984fd8573cd171e32831c894be81eee6569c
toasterofbread <talohalton@gmail.com>
2024-10-15 20:28:07 +0100

    Fix font change not applying, apply change immediately

--- buildSrc/src/main/kotlin/plugins/spmp/Dependencies.kt
+++ buildSrc/src/main/kotlin/plugins/spmp/Dependencies.kt
@@ -40,7 +40,7 @@ class SpMpDeps(extra: Map<String, Any>) {
                 license_url = "https://github.com/toasterofbread/spmp-server/blob/6dde651ffc102d604ac7ecd5ac7471b1572fd2e6/LICENSE"
             ),
             "dev.toastbits.composekit" to DependencyInfo(
-                version = "c92d43203c",
+                version = "4c7b8690ff",
                 name = "ComposeKit",
                 author = "toasterofbread",
                 url = "https://github.com/toasterofbread/composekit",

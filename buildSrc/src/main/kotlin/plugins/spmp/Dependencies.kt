58e2b5771fdc543273f921011504cb8173b2d883
toasterofbread <talohalton@gmail.com>
2024-10-16 19:05:26 +0100

    Improve error produced when PlatformPreferences.android deserialisation fails

--- buildSrc/src/main/kotlin/plugins/spmp/Dependencies.kt
+++ buildSrc/src/main/kotlin/plugins/spmp/Dependencies.kt
@@ -40,7 +40,7 @@ class SpMpDeps(extra: Map<String, Any>) {
                 license_url = "https://github.com/toasterofbread/spmp-server/blob/6dde651ffc102d604ac7ecd5ac7471b1572fd2e6/LICENSE"
             ),
             "dev.toastbits.composekit" to DependencyInfo(
-                version = "610ce0b747",
+                version = "06d81b91ad",
                 name = "ComposeKit",
                 author = "toasterofbread",
                 url = "https://github.com/toasterofbread/composekit",
